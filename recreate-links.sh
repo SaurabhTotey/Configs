7fcbce684be2ad3580fc8edd0f64658651df7b15
Saurabh Totey <SaurabhTotey@gmail.com>
2021-03-02 12:05:03 -0700

    First attempt at collecting configs

--- recreate-links.sh
+++ recreate-links.sh
@@ -8,9 +8,12 @@ do
 	name=$(echo $module | jq -r '.name')
 	source_path_string=$(echo $module | jq -r '.path')
 	source_path=${source_path_string/#~/$HOME}
+	target_path=configs/$name
 	if [ -d $source_path ]; then
-		echo "dir"
+		ln -s $source_path $target_path
 	else
-		echo "file"
+		mkdir $target_path
+		let file_name=${source_path##*/}
+		ln -s $source_path $target_path/$file_name
 	fi
 done
