da0b13c7194981550ebc266c4f61e6bab3790dbc
Saurabh Totey <SaurabhTotey@gmail.com>
2021-03-02 11:36:12 -0700

    Got correct file/directory checks

--- recreate-links.sh
+++ recreate-links.sh
@@ -5,7 +5,12 @@ mkdir configs
 
 for module in $(jq -c '.[]' modules.json)
 do
-	name="$(echo "$module" | jq '.name')"
-	source_path="$(echo "$module" | jq '.path')"
-	echo "$name $source_path"
+	name=$(echo $module | jq -r '.name')
+	source_path_string=$(echo $module | jq -r '.path')
+	source_path=${source_path_string/#~/$HOME}
+	if [ -d $source_path ]; then
+		echo "dir"
+	else
+		echo "file"
+	fi
 done
