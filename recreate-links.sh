a0300bda3d55eab3c2d92309604c835932739cad
Saurabh Totey <SaurabhTotey@gmail.com>
2021-03-05 18:07:40 -0700

    Correct symlinks for 'hidden' files and more useful setup-dev

--- recreate-links.sh
+++ recreate-links.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for module in $(jq -c '.[]' modules.json)
-do
+for module in $(jq -c '.[]' modules.json); do
 	name=$(echo "$module" | jq -r '.name')
 	target_path=$(echo "$module" | jq -r '.path')
 	target_path=${target_path/#~/$HOME}
 	cd configs/"$name" || exit
+	shopt -s dotglob
 	for file_to_symlink in *; do
-		sudo ln -sf "$(pwd)/$file_to_symlink" "$target_path"
+		sudo ln -sf "$(pwd)/$file_to_symlink" "$target_path/$file_to_symlink"
 	done
+	shopt -u dotglob
 	cd ../..
 done
