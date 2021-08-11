6d0753a841c9e908202ac984b488873734204d86
Saurabh Totey <SaurabhTotey@gmail.com>
2021-08-10 22:38:07 -0600

    Copied SideDecoration from https://gist.github.com/Avaq/9691fa3f8b6538fb949570884e5ee91e

--- configs/XMonad/xmonad.hs
+++ configs/XMonad/xmonad.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
 import XMonad
 import XMonad.Actions.SpawnOn
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.Decoration
 import XMonad.Layout.Spacing
+import XMonad.StackSet (integrate)
 import XMonad.Util.EZConfig
 import XMonad.Util.SpawnOnce
 import Graphics.X11.ExtraTypes.XF86
@@ -15,6 +18,24 @@ myStartupHook = do
 	spawnOnOnce "social" "spotify"
 	spawnOnOnce "social" "slack"
 	spawnOnce "polybar -r default"
+
+data SideDecoration a = SideDecoration Direction2D deriving (Show, Read)
+
+instance Eq a => DecorationStyle SideDecoration a where
+
+	shrink b (Rectangle _ _ dw dh) (Rectangle x y w h)
+		| SideDecoration U <- b = Rectangle x (y + fi dh) w (h - dh)
+		| SideDecoration R <- b = Rectangle x y (w - dw) h
+		| SideDecoration D <- b = Rectangle x y w (h - dh)
+		| SideDecoration L <- b = Rectangle (x + fi dw) y (w - dw) h
+
+	pureDecoration b dw dh _ st _ (win, Rectangle x y w h)
+		| win `elem` integrate st && dw < w && dh < h = Just $ case b of
+			SideDecoration U -> Rectangle x y w dh
+			SideDecoration R -> Rectangle (x + fi (w - dw)) y dw h
+			SideDecoration D -> Rectangle x (y + fi (h - dh)) w dh
+			SideDecoration L -> Rectangle x y dw h
+		| otherwise = Nothing
 
 myLayouts = myDefaultLayouts
 	where
